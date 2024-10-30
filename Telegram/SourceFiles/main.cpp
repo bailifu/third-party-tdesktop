54c55d2d9b890b85c993d88312d4251709fb4976
Baili Fu <contact@zh3z.sbs>
2024-10-30 15:19:44 +0800

    UP

--- Telegram/SourceFiles/main.cpp
+++ Telegram/SourceFiles/main.cpp
@@ -9,6 +9,5 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 int main(int argc, char *argv[]) {
   const auto launcher = Core::Launcher::Create(argc, argv);
-  printf("launcher: %p\n", launcher);
   return launcher ? launcher->exec() : 1;
 }
