6546e9ec733706aa41932344ffadd035038d58c4
Baili Fu <contact@zh3z.sbs>
2024-10-30 23:35:51 +0800

    Revert "FIX"
    This reverts commit 92dc2ebfd20fdfd96a4dfb879b62730142587171.

--- Telegram/SourceFiles/core/launcher.cpp
+++ Telegram/SourceFiles/core/launcher.cpp
@@ -563,9 +563,7 @@ int Launcher::executeApplication() {
 
 void Launcher::checkAndSetLanguage() {
   constexpr auto kDefaultLanguage = "zh-hans-beta"_cs;
-  Lang::Language language;
-  language.id = kDefaultLanguage.utf16();
-  Lang::GetInstance().switchToId(language);
+  Lang::GetInstance().switchToId(kDefaultLanguage);
 }
 
 }  // namespace Core
