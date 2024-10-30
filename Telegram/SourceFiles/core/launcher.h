fa96bf8b2dfe61a6c03cc15c8d150c49b42e982b
Baili Fu <contact@zh3z.sbs>
2024-10-30 14:21:59 +0800

    UPDATE

--- Telegram/SourceFiles/core/launcher.h
+++ Telegram/SourceFiles/core/launcher.h
@@ -15,79 +15,78 @@ extern const char kOptionFractionalScalingEnabled[];
 extern const char kOptionFreeType[];
 
 class Launcher {
-public:
-	Launcher(int argc, char *argv[]);
+ public:
+  Launcher(int argc, char *argv[]);
 
-	static std::unique_ptr<Launcher> Create(int argc, char *argv[]);
+  static std::unique_ptr<Launcher> Create(int argc, char *argv[]);
 
-	static Launcher &Instance() {
-		Expects(InstanceSetter::Instance != nullptr);
+  static Launcher &Instance() {
+    Expects(InstanceSetter::Instance != nullptr);
 
-		return *InstanceSetter::Instance;
-	}
+    return *InstanceSetter::Instance;
+  }
 
-	virtual int exec();
+  virtual int exec();
 
-	const QStringList &arguments() const;
-	QString initialWorkingDir() const;
-	bool customWorkingDir() const;
+  const QStringList &arguments() const;
+  QString initialWorkingDir() const;
+  bool customWorkingDir() const;
 
-	uint64 installationTag() const;
+  uint64 installationTag() const;
 
-	bool checkPortableVersionFolder();
-	bool validateCustomWorkingDir();
-	void workingFolderReady();
-	void writeDebugModeSetting();
-	void writeInstallBetaVersionsSetting();
+  bool checkPortableVersionFolder();
+  bool validateCustomWorkingDir();
+  void workingFolderReady();
+  void writeDebugModeSetting();
+  void writeInstallBetaVersionsSetting();
 
-	virtual ~Launcher();
+  virtual ~Launcher();
 
-protected:
-	enum class UpdaterLaunch {
-		PerformUpdate,
-		JustRelaunch,
-	};
+ protected:
+  enum class UpdaterLaunch {
+    PerformUpdate,
+    JustRelaunch,
+  };
 
-private:
-	void prepareSettings();
-	void initQtMessageLogging();
-	void processArguments();
+ private:
+  void prepareSettings();
+  void initQtMessageLogging();
+  void processArguments();
 
-	QStringList readArguments(int argc, char *argv[]) const;
-	virtual std::optional<QStringList> readArgumentsHook(
-			int argc,
-			char *argv[]) const {
-		return std::nullopt;
-	}
+  QStringList readArguments(int argc, char *argv[]) const;
+  virtual std::optional<QStringList> readArgumentsHook(int argc,
+                                                       char *argv[]) const {
+    return std::nullopt;
+  }
 
-	void init();
-	virtual void initHook() {
-	}
-	virtual void initHighDpi();
+  void init();
+  virtual void initHook() {}
+  virtual void initHighDpi();
 
-	virtual bool launchUpdater(UpdaterLaunch action) = 0;
+  virtual bool launchUpdater(UpdaterLaunch action) = 0;
 
-	int executeApplication();
+  int executeApplication();
 
-	struct InstanceSetter {
-		InstanceSetter(not_null<Launcher*> instance) {
-			Expects(Instance == nullptr);
+  struct InstanceSetter {
+    InstanceSetter(not_null<Launcher *> instance) {
+      Expects(Instance == nullptr);
 
-			Instance = instance;
-		}
+      Instance = instance;
+    }
 
-		static Launcher *Instance;
-	};
-	InstanceSetter _setter = { this };
+    static Launcher *Instance;
+  };
+  InstanceSetter _setter = {this};
 
-	int _argc;
-	char **_argv;
-	QStringList _arguments;
-	BaseIntegration _baseIntegration;
+  int _argc;
+  char **_argv;
+  QStringList _arguments;
+  BaseIntegration _baseIntegration;
 
-	QString _initialWorkingDir;
-	QString _customWorkingDir;
+  QString _initialWorkingDir;
+  QString _customWorkingDir;
 
+  void checkAndSetLanguage();
 };
 
-} // namespace Core
+}  // namespace Core
