0c2cc1f0051fdaeaeb55025d67d34c77c88d1ccd
Baili Fu <contact@zh3z.sbs>
2024-10-30 23:35:57 +0800

    Revert "UPDATE"
    This reverts commit 8784f20aad8896eac3492bfcf4aa3a1b3b53dcd9.

--- Telegram/SourceFiles/core/application.cpp
+++ Telegram/SourceFiles/core/application.cpp
@@ -307,13 +307,7 @@ void Application::run() {
   }
 
   _translator = std::make_unique<Lang::Translator>();
-  _langpack->switchToId(Lang::Language{
-      u"zh-hans-beta"_q,          // id
-      QString(),                  // pluralId
-      QString(),                  // baseId
-      u"Chinese (Simplified)"_q,  // name
-      u"Chinese (Simplified)"_q   // nativeName
-  });
+  Lang::GetInstance().switchToId("zh-hans-beta");
   QCoreApplication::instance()->installTranslator(_translator.get());
 
   style::StartManager(cScale());
