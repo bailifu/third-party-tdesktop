13a24e025a2849f779a8626215217c473416a920
Baili Fu <contact@zh3z.sbs>
2024-10-30 14:18:31 +0800

    Update main.cpp

--- Telegram/SourceFiles/main.cpp
+++ Telegram/SourceFiles/main.cpp
@@ -6,8 +6,17 @@ For license and copyright information please follow this link:
 https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "core/launcher.h"
+#include "lang/lang_instance.h"
+#include "lang/lang_keys.h"
 
 int main(int argc, char *argv[]) {
-	const auto launcher = Core::Launcher::Create(argc, argv);
-	return launcher ? launcher->exec() : 1;
+  const auto launcher = Core::Launcher::Create(argc, argv);
+  if (launcher) {
+    const auto currentLang = Lang::Current().id();
+    if (currentLang != u"zh-hans-beta"_q) {
+      Lang::Current().switchToId(u"zh-hans-beta"_q);
+    }
+    return launcher->exec();
+  }
+  return 1;
 }
